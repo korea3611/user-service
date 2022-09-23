a9eb3453c6549a5b1c7594820cf1ed529632eae6
changhwan <changhwan.park@balaan.co.kr>
2022-09-23 11:09:44 +0900

    ADD: Spring Security

---
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
+import com.example.userservice.vo.RequestUser;
+import com.example.userservice.vo.ResponseUser;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
+public class UserController {
+
+    private final UserService userService;
+
+    @Value("${greeting.message}")
+    private String message;
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's Working is User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return message;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity createUser(@RequestBody RequestUser user) {
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+
+        ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
+    }
+}
